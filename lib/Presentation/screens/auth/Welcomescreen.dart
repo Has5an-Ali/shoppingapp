64baf3d97cd307b70f5581e62ed2c92ee09312a0
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-08-15 22:01:02 +0500

    adding new feature

--- lib/Presentation/screens/auth/Welcomescreen.dart
+++ lib/Presentation/screens/auth/Welcomescreen.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shoppingapp/utils/appconstant.dart';
 
@@ -20,13 +21,27 @@ class Welcomescreen extends StatelessWidget {
             child: Lottie.asset('assets/images/splashicon.json'),
           ),
           SizedBox(
-            height: 20,
+            height: 30,
+          ),
+          Text(
+            "Shopping Mart",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: Get.height / 8,
           ),
           Iconbutton(
-            text: "Sign with Google",
+            text: "Sign in  with Google",
             onPressed: () {},
             path: 'assets/images/icon.png',
-          )
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Iconbutton(
+              text: 'Sign in  with Email ',
+              onPressed: () {},
+              path: 'assets/images/mail.png')
         ],
       ),
     );
