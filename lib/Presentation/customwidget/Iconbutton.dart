d846f677108b98b87d832f7afbd6c5455a03f41c
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-08-15 21:39:44 +0500

    icon button edit

--- lib/Presentation/customwidget/Iconbutton.dart
+++ lib/Presentation/customwidget/Iconbutton.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shoppingapp/utils/appconstant.dart';
 
 class Iconbutton extends StatelessWidget {
   final String text;
@@ -14,7 +16,24 @@ class Iconbutton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextButton.icon(
-        icon: Image.asset(path), onPressed: onPressed, label: Text(text));
+    return SizedBox(
+      height: 50,
+      width: 260,
+      child: TextButton.icon(
+          style: TextButton.styleFrom(
+              backgroundColor: AppConstant.appmaincolor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6))),
+          icon: Image.asset(
+            path,
+            height: Get.height / 12,
+            width: Get.width / 12,
+          ),
+          onPressed: onPressed,
+          label: Text(
+            text,
+            style: const TextStyle(color: AppConstant.textcolor),
+          )),
+    );
   }
 }
