73ffa73d314c2973e662683e4dc885fd9ab050c3
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-08-23 08:49:21 +0500

    dataadd

--- lib/controller/signinwithemail.dart
+++ lib/controller/signinwithemail.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shoppingapp/model/usermodel.dart';
 import 'package:shoppingapp/utils/appconstant.dart';
 
 class Signinwithemail extends GetxController {
@@ -13,10 +14,32 @@ class Signinwithemail extends GetxController {
   Future<UserCredential?> SigninMethod(
     String Useremail,
     String Username,
-    String Usercity,
+    String Userphone,
     String Userpassword,
+    String userDeviceToken,
   ) async {
-    try {} on FirebaseAuthException catch (e) {
+    try {
+      UserCredential userCredential =
+          await _auth.createUserWithEmailAndPassword(
+              email: Useremail, password: Userpassword);
+
+      await userCredential.user!.sendEmailVerification();
+
+      UserModel userModel = UserModel(
+          uId: userCredential.user!.uid,
+          username: Username,
+          email: Useremail,
+          phone: Userphone,
+          userImg: '',
+          userDeviceToken: userDeviceToken,
+          country: '',
+          userAddress: '',
+          street: '',
+          isAdmin: false,
+          isActive: true,
+          createdOn: DateTime.now(),
+          city: '');
+    } on FirebaseAuthException catch (e) {
       Get.snackbar("Error", "$e",
           snackPosition: SnackPosition.BOTTOM,
           backgroundColor: AppConstant.appmaincolor,
