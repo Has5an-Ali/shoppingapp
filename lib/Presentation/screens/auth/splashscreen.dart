cedc28f8ecc0bb9a69d3beae4f35f5bdacf122d4
Hassan Ali <122170160+Has5an-Ali@users.noreply.github.com>
2024-08-31 21:45:14 +0500

    update

--- lib/Presentation/screens/auth/splashscreen.dart
+++ lib/Presentation/screens/auth/splashscreen.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors
+
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
+import 'package:shoppingapp/Presentation/screens/user-panel/Main-screen.dart';
 import 'package:shoppingapp/utils/appconstant.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../../controller/Getuserdata.dart';
+import '../admin_panel/AdminMainScreen.dart';
 import 'Welcomescreen.dart';
 import 'signinscreen.dart';
 
@@ -15,6 +20,7 @@ class Splashscreen extends StatefulWidget {
 }
 
 class _SplashscreenState extends State<Splashscreen> {
+  User? user = FirebaseAuth.instance.currentUser;
   @override
   void initState() {
     super.initState();
@@ -22,9 +28,24 @@ class _SplashscreenState extends State<Splashscreen> {
     Timer(
       const Duration(seconds: 6),
       () {
-        Get.off(() => Welcomescreen());
+        Checklogin(context);
       },
     );
+  }
+
+  Future<void> Checklogin(BuildContext context) async {
+    if (user != null) {
+      final GetUserDataController getUserDataController =
+          Get.put(GetUserDataController());
+      var UserData = await getUserDataController.GetdataMethod(user!.uid);
+      if (UserData[0]['isAdmin'] == true) {
+        Get.offAll(() => Adminscreen());
+      } else {
+        Get.offAll(() => Mainscreen());
+      }
+    } else {
+      Get.offAll(() => Welcomescreen());
+    }
   }
 
   @override
